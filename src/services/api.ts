9238970f12f57a2899f99869a94687ebf5fe8c0f
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-09-12 00:12:19 -0300

    Fix: Removed outdated API

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app/api/efood'
+    baseUrl: 'https://ebac-fake-api.vercel.app/api/efood'
   }),
   endpoints: (builder) => ({
     getRestaurant: builder.query<RestaurantInterface[], void>({
